9ff75e188d5f743f44f19089d58c0df6c7b3d3d3
Dima <dmytrovelychko@outlook.com>
2025-06-01 12:26:10 +0200

    Implemented DTO's

---
+++ api/Dtos/TaskDto.cs
@@ -0,0 +1,11 @@
+﻿namespace api.Dtos
+{
+    public class TaskDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+
+        public List<TagDto> Tags { get; set; } = new List<TagDto>();
+    }
+}
