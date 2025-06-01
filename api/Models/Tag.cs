198b4d8772774570d8dbd19a23d7dd723dfee57c
Dima <dmytrovelychko@outlook.com>
2025-06-01 12:20:58 +0200

    Implemented Task, Tag & TaskTag Models

---
+++ api/Models/Tag.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace api.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        [JsonIgnore] // Avoid sending this list to frontend
+        public List<TaskTag> TaskTags { get; set; } = new List<TaskTag>();
+    }
+}
---
+++ api/Models/Task.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace api.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; } // Task description can be left empty
+
+        [JsonIgnore] // Avoid sending this list to frontend
+        public List<TaskTag> TaskTags { get; set; } = new List<TaskTag>();
+    }
+}
